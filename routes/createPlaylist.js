8210ff23d91d182785a8106620c2847414d644cc
Christian Carnley <ccarnley7@gmail.com>
2015-04-02 14:50:19 -0600

    Trying to get the token from spotify

--- routes/createPlaylist.js
+++ routes/createPlaylist.js
@@ -1,23 +1,30 @@
 var express = require('express');
 var request = require('request');
 var http = require('http');
+var url = require('url');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-    /*http.get('https://api.spotify.com/v1/me')
-
+    //http.get('https://api.spotify.com/v1/me')
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
     request.post(
-        'https://api.spotify.com/v1/users/{user_id}/playlists',
-        { form: { key: 'value' } },
+        'https://accounts.spotify.com/api/token',
+        {
+            grant_type: "authorization_code",
+            code: query,
+            redirect_uri: "http://carnley.me/createPlaylist"
+        },
         function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log(body)
             }
         }
     );
-    request.get()*/
+    //request.get()
     res.send("test")
 });
 

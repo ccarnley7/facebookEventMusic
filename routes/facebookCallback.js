0add96b214a1ec699f2552f303c9360301da9b44
Christian Carnley <ccarnley7@gmail.com>
2015-04-07 13:01:43 -0600

    reverting passport moves

--- routes/facebookCallback.js
+++ routes/facebookCallback.js
@@ -1,19 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport'), FacebookStrategy = require('passport-facebook').Strategy;
-
-
-passport.use(new FacebookStrategy({
-        clientID: '377801185740004',
-        clientSecret: '12784ed027b4b2b2fed1cb15d47a9c54',
-        callbackURL: "http://www.carnley.me/facebookCallback"
-    },
-    function(accessToken, refreshToken, profile, done) {
-        console.log("Facebook token", accessToken);
-        console.log("Facebook profile", profile);
-        done(null, profile);
-    }
-));
+var passport = require('passport');
 
 /* GET users listing. */
 router.get('/', passport.authenticate('facebook'));
